1a98cbe11faf48edd31ae4f58f90f2aa7f2d0259
Sxturn7 <satvikbhukya07@gmail.com>
2026-03-05 21:32:36 +0530

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -4,29 +4,30 @@ let ok = 0
 let text1 = "Hey Prozac ❤️"
 let text2 = "A small universe from Saturn to you."
 
-function typeWriter(text,id){
+function typeWriter(text, elementId){
 
-if(i < text.length){
+    if(i < text.length){
 
-document.getElementById(id).innerHTML += text.charAt(i)
-i++
+        document.getElementById(elementId).innerHTML += text.charAt(i)
+        i++
 
-}else{
+    } else {
 
-i = 0
-ok++
+        i = 0
+        ok++
 
-}
+    }
 
 }
 
 setInterval(function(){
 
-if(ok == 0){
-typeWriter(text1,"txt1")
-}
-else if(ok == 1){
-typeWriter(text2,"txt2")
-}
+    if(ok === 0){
+        typeWriter(text1,"txt1")
+    }
+
+    else if(ok === 1){
+        typeWriter(text2,"txt2")
+    }
 
 },100)
